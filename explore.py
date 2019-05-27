b6d4cdd30b8aa4e7b96d84bdd20cc26e2e9e643e
theDoctor2013 <943024256@qq.com>
2019-05-27 21:50:49 +0800

    deal file error in train.csv

---
+++ explore.py
@@ -0,0 +1,16 @@
+import pandas as pd
+
+
+def pre():
+    with open('data/train_old.csv','r') as f:
+        with open('data/train.csv','w') as fo:
+            for line in f:
+                line.replace('\r','')
+                fo.write(line)
+
+
+
+test_df = pd.read_csv('data/20190527_test.csv')
+train_df = pd.read_csv('data/train.csv')
+train_df = train_df.dropna()
+print(train_df.info())
